7b77c221cfb244fa300bbac4a536a7623ad88824
Nicolas Beun <nicolas.beun@gmail.com>
2022-09-05 10:43:49 +0200

    Récupération test des éléments de la base de donnée

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<?php ?>
+<?php
+// Connexion base de donnée
+$dsn = "mysql:host=localhost;dbname=literie3000;cahrset=UTF8";
+$db = new PDO($dsn, "root", "");
+
+// Récupération des matelas
+$query = $db->query("SELECT * FROM matelas");
+// Récupération sous forme d'un tableau associatif
+$matelas = $query->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<h1>Nos Matelas</h1>
+<div class="matelas">
+    <?php
+    foreach ($matelas as $matela) {
+    ?>
+        <div class="matela">
+            <h2><?= $matela["nom"] ?></h2>
+        </div>
+    <?php
+    } ?>
+</div>
